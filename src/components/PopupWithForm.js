6ac50e30dd5988a903483a5aad597aea52fd469a
James Penera <jamespenera@icloud.com>
2023-09-20 21:57:53 -0700

    update

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ import PopUp from "./PopUp.js";
 
 export default class PopUpWithForm extends PopUp {
   constructor(popupSelector, handleFormSubmit) {
-    super({popupSelector});
+    super(popupSelector);
     this._popForm = this._popupElement.querySelector(".modal__form");
     this._popSubmitBtn = this._popForm.querySelector(".modal__button_submit");
     this._popInputs = this._popForm.querySelectorAll(".modal__input");
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,19 +1,20 @@
 import PopUp from "./PopUp.js";
 
 export default class PopUpWithImage extends PopUp {
-  constructor({popUpSelector}) {
+  constructor(popUpSelector) {
     super(popUpSelector);
     this._image = this._popupElement.querySelector(".modal__previewImage");
     this._caption = this._popupElement.querySelector("#preview-title"); 
   }
   
-  open(name,link) {
+  open(data) {
+    super.open();
+    if (data) {
       this._image.src = data.link; 
       this._caption.alt = data.name; 
       this._caption.textContent = data.name; 
-      super.open();
     }
   }
-
+}
 
 //export default PopUpWithImage;
